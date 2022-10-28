3f7d63d40d83da77eb37740176413a7997399157
andres <andres.martin.bricenio@gmail.com>
2022-10-27 22:06:38 -0300

    agregando offcanvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 class Producto {
   constructor(id, nombre, descripcion, precio, imagen) {
     this.id = id;
@@ -108,7 +106,7 @@ const productos = [
 ];
 console.log(productos);
 
-const carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 const productosContainer = document.getElementById("productosContainer");
 
@@ -124,13 +122,113 @@ const mostrarProductos = () => {
         <h4 class="card-title">${producto.nombre}</h3>
         <p class="card-text">${producto.descripcion}</p>
         <h5 class="card-text">$${producto.precio}</h5>
-        <button class="btn btn-success" id="boton${producto.id}">Añadir</button>
+        <button class="btn btn-success my-3" id="boton${producto.id}">Añadir</button>
+      </div>
+    </div>
+    `;
+    // TODO: HACER INCREMENTO Y DECREMENTO DE CANTIDAD
+    productosContainer.appendChild(card);
+
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.onclick = () => {
+      Toastify({
+        text: `Se agrego el producto al carrito`,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "left",
+        stopOnFocus: true,
+        style: {
+          background: "green",
+        },
+      }).showToast();
+
+      agregarAlCarrito(producto.id);
+    };
+  });
+};
+
+const agregarAlCarrito = (id) => {
+  const producto = productos.find((producto) => producto.id === id);
+  const existeProducto = carrito.find((producto) => producto.id === id); //TODO: ver si se puede hacer ternario
+  if (existeProducto) {
+    existeProducto.cantidad++;
+    mostrarCarrito();
+  } else {
+    carrito.push(producto);
+    mostrarCarrito();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }
+};
+
+mostrarProductos();
+
+// carrito
+const carritoContainer = document.getElementById("carritoContainer");
+
+const mostrarCarrito = () => {
+  carritoContainer.innerHTML = ""; //Para evitar duplicidad en carrito
+
+  carrito.forEach((producto) => {
+    const card = document.createElement("div");
+    card.classList.add("col-xl-3", "col-md-6", "col-xs-12");
+    card.innerHTML = `
+    <div class="card">
+      <img src="${producto.imagen}" class="card-img-top imagen-producto" alt="${
+      producto.nombre
+    }">
+      <div class="card-body">
+      <h4 class="card-title">${producto.nombre}</h3>
+        <p class="card-text">${producto.descripcion}</p>
+        <p class="card-text">Cantidad: ${producto.cantidad}</p>
+        <h5 class="card-text">$${producto.precio * producto.cantidad}</h5>
+        <button class="btn btn-danger my-3" id="eliminar${
+          producto.id
+        }">Eliminar</button>
       </div>
     </div>
     `;
 
-    productosContainer.appendChild(card);
+    carritoContainer.appendChild(card);
+
+    const boton = document.getElementById(`eliminar${producto.id}`);
+    boton.onclick = () => {
+      eliminarProducto(producto.id);
+    };
   });
+  calcularTotal();
 };
 
-mostrarProductos();
+const eliminarProducto = (id) => {
+  const producto = productos.find((producto) => producto.id === id);
+  const index = carrito.indexOf(producto);
+  carrito.splice(index, 1);
+  mostrarCarrito();
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.onclick = () => {
+  eliminarTodoElCarrito();
+};
+
+const eliminarTodoElCarrito = () => {
+  carrito.splice(0, carrito.length);
+  console.log(carrito.length);
+  mostrarCarrito();
+  localStorage.clear();
+};
+
+const total = document.getElementById("total");
+
+const calcularTotal = () => {
+  let totalCompra = 0;
+  carrito.forEach((producto) => {
+    totalCompra += producto.precio * producto.cantidad;
+  });
+  total.innerHTML = `$${totalCompra}`;
+};
